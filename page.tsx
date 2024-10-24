bf4e4eab7af244f77ea3adb3e2d17746f98ba395
Mirmuhammadbhanbhro <151605191+Mirmuhammadbhanbhro@users.noreply.github.com>
2024-10-24 01:41:06 -0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import Hero from "./component/hero";
+import Contact from "./component/contact";
+import Project from "./component/project";
+import About from "./component/about";
+import Skill from "./component/skill";
+export default function Home() {
+  return (
+    <div id="navbar">
+      <Hero/>
+      <About/>
+      <Contact/>
+      <Project/>
+      <Skill/>
+    </div>
+  );
+}
